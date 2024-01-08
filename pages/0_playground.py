7bcfd7ee2597f3b861e7ca68ab90024103589aa5
Crowmium1 <123245410+Crowmium1@users.noreply.github.com>
2024-01-08 15:10:43 +0000

    testing out the basics

--- pages/0_playground.py
+++ pages/0_playground.py
@@ -1,4 +1,12 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
+
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
+
+st.dataframe(dataframe.style.highlight_max(axis=0))
 
 st.write('Hello world!')
 
@@ -8,3 +16,16 @@ if st.button('Say hello'):
      st.write('Why hello there')
 else:
      st.write('Goodbye')
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+
+
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
